120630b13b7544f21cef097d6997a369b34ca1b6
Anilrocky <anilkumarac96@gmail.com>
2023-04-12 16:45:58 +0530

    [Anil] Add:Add contacts to Address book

--- AddressBook/AddressBookMain.cs
+++ AddressBook/AddressBookMain.cs
@@ -8,6 +8,42 @@ namespace AddressBook
 {
     public class AddressBookMain
     {
-
+        List<Contacts> addressList = new List<Contacts>();
+        public AddressBookMain()
+        {
+            Contacts contact1 = new Contacts()
+            {
+                FirstName = "Anil",
+                LastName = "Kumar",
+                Address = "Bangalore",
+                City = "Bangalore",
+                State = "Karnataka",
+                ZIP = "563123",
+                PhoneNumber = "6789012345",
+                Email = "a@gamil.com"
+            };
+            Contacts contact2 = new Contacts()
+            {
+                FirstName = "Soma",
+                LastName = "Shekar",
+                Address = "Bangalore",
+                City = "Bangalore",
+                State = "Karnataka",
+                ZIP = "563123",
+                PhoneNumber = "6789012345",
+                Email = "s@gamil.com"
+            };
+            addressList.Add(contact1);
+            addressList.Add(contact2);
+        }
+        public void Display()
+        {
+            foreach (var data in addressList)
+            {
+                Console.WriteLine("First name: " + data.FirstName + "\n" + "Last name: " + data.LastName + "\n" +
+                      "Address: " + data.Address + "\n" + "City: " + data.City + "\n" + "State: " + data.State + "\n" +
+                    "ZIP: " + data.ZIP + "\n" + "Phone number: " + data.PhoneNumber + "\n" + "E-Mail: " + data.Email);
+            }
+        }
     }
 }
--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -6,6 +6,22 @@ namespace AddressBook
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book problem");
+            AddressBookMain address = new AddressBookMain();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("\nChoose option to perform \n1.Create contacts in address book \n2.Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        address.Display();
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
+            }           
         }
     }
 }
